db39ae7ba0eeee99f6dcbb2e309649e7e8b474cf
ondergenc <ondergenc@gmail.com>
2022-06-05 11:44:33 +0300

    Added new solution. And added server and client projects

---
+++ grpc/server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using Grpc.Core;
+
+namespace server
+{
+    class Program
+    {
+        const int Port = 50051;
+
+        static void Main(string[] args)
+        {
+            Server server = null;
+
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+                server.Start();
+                Console.WriteLine("The server is listening on the port : " + Port);
+                Console.ReadKey();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start : " + e.Message);
+                throw;
+            }
+            finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
+        }
+    }
+}
+
